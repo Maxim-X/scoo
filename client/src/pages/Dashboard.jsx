a21be9e698c448619bcfdde7ca9f46435ab5ba7e
Maxim-X <43073464+Maxim-X@users.noreply.github.com>
2022-08-20 02:46:05 +0500

    Edit

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,10 +1,57 @@
 import React from 'react';
+import {Col, Container, Row} from "react-bootstrap";
+import {AiFillExperiment} from "react-icons/ai";
 
 const Dashboard = () => {
     return (
-        <div>
-            <h1>Dashboard</h1>
-        </div>
+        <Container fluid>
+            <Row>
+                <Col md={3}>
+                    <div className="left_main_menu">
+                        <ul className="nav flex-column left-menu">
+                            <li className="nav-item left-menu-li">
+                                <a className="nav-link active" href="#">
+                                    <span><AiFillExperiment size="1.4rem" /></span>
+                                    <span>Пункт меню</span>
+                                </a>
+                            </li>
+                            <ul>
+                                <li className="nav-item left-menu-li">
+                                    <a className="nav-link active" href="#">
+                                        <span><AiFillExperiment size="1.4rem" /></span>
+                                        <span>Пункт меню</span>
+                                    </a>
+                                </li>
+                                <li className="nav-item left-menu-li">
+                                    <a className="nav-link active" href="#">
+                                        <span><AiFillExperiment size="1.4rem" /></span>
+                                        <span>Пункт меню</span>
+                                    </a>
+                                </li>
+                            </ul>
+                            <li className="nav-item left-menu-li">
+                                <a className="nav-link active" href="#">
+                                    <span><AiFillExperiment size="1.4rem" /></span>
+                                    <span>Пункт меню</span>
+                                </a>
+                            </li>
+                            <li className="nav-item left-menu-li">
+                                <a className="nav-link active" href="#">
+                                    <span><AiFillExperiment size="1.4rem" /></span>
+                                    <span>Пункт меню</span>
+                                </a>
+                            </li>
+                            <li className="nav-item left-menu-li">
+                                <a className="nav-link active" href="#">
+                                    <span><AiFillExperiment size="1.4rem" /></span>
+                                    <span>Пункт меню</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
